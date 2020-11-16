afc4698fd89990ebbf6914aae54785cac0c89bdf
josilob <bjosilo@gmail.com>
2020-11-16 11:07:36 -0500

    schema, migration and seed

--- tunr_mini_api/db/seeds.rb
+++ tunr_mini_api/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ Song.create([
+    { title: 'Kashmir', artist: "Led Zeppelin", time:"8:31"  , fav:false },
+    { title: 'All along the watchtower', artist: "Jimi Hendrix", time:"4:01"  , fav:false },
+    { title: 'We will rock you', artist: "Queen", time:"2:14"  , fav:false },
+    { title: 'Whole lotta love', artist: "Led Zeppelin", time:"5:34"  , fav:false },
+    { title: 'Roxanne', artist: "The Police", time:"3:10"  , fav:false },
+    { title: 'Here I go again', artist: "Whitesnake", time:"4:31"  , fav:false },
+    { title: 'Jack and Dianne', artist: "John Mellencamp", time:"4:17"  , fav:false },
+    { title: 'Dont stop believin', artist: "Journey", time:"4:10"  , fav:false },
+    { title: 'American Girl', artist: "Tom Petty", time:"3:32"  , fav:false },
+    { title: 'Rebell yell', artist: "Billy Idol", time:"4:45"  , fav:false },
+  ])
